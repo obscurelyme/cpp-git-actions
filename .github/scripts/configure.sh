a83e9d158da67825675404aead681d037a4cc6bd
obscurelyme <nico.frozencoffee@gmail.com>
2021-11-15 15:55:14 -0800

    test workflow

---
+++ .github/scripts/configure.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cmake --no-warn-unused-cli \
+  -DCMAKE_TOOLCHAIN_FILE="~/vcpkg/scripts/buildsystems/vcpkg.cmake" \
+  -DCMAKE_C_COMPILER:FILEPATH="/usr/bin/clang " \
+  -DCMAKE_CXX_COMPILER:FILEPATH="/usr/bin/clang++" \
+  -B ./build \
+  -G Ninja
